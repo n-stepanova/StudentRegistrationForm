494504e84ed201a272adeb0ad1e10d337a863ac2
n-stepanova <nstepanovaqa@gmail.com>
2023-10-05 21:27:24 +0300

    поиск локаторов

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -2,10 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
